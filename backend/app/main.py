7de422fcbda0d01d44227c209dbe77b367d758d6
Jessie Jalca <jessiejalca@gmail.com>
2026-04-02 23:45:55 -0400

    feat: add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import songs
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = FastAPI(title="Reprise")
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
