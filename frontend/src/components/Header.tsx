8bda9271c2fed7b08727dd43423fae34c00e7f78
Jessie Jalca <jessiejalca@gmail.com>
2026-06-01 00:02:41 -0400

    feat: add search input with URL query params to Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,33 @@
 import { IoSearch } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 function Header() {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    console.log(searchParams)
+
+    const makeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Add the query to the search params for reading
+        setSearchParams(`?q=${e.target.value}`)
+    }
+
     return (
         <header id='banner'>
             <Link to='/' className="logo">Reprise</Link>
             <div className='search-input-wrapper'>
                 <IoSearch size={16} className='icon' />
-                <input id="searchbar" type="text" placeholder="Search songs or artists..." />
+                <input 
+                    id="searchbar" 
+                    type="text" 
+                    placeholder="Search songs or artists..."  
+                    value={searchParams.get("q") ?? ""} 
+                    onChange={makeQuery} 
+                />
             </div>
             <div></div>
         </header>
-    )
+    )   
 }
 
 export default Header
